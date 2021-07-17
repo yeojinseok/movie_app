f498682ded7c09826c3b2618c6ad160938f106f3
yeojinseok <wlstjrghdud@naver.com>
2021-07-17 15:19:28 +0900

    Updates

---
+++ precache-manifest.d80ef3a4fd63600f749a33461d02af39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb449f7ce64943777ae77cc6f3cd524",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "66de514a7cc916716eb3",
+    "url": "/movie_app/static/css/main.92e86375.chunk.css"
+  },
+  {
+    "revision": "1e9944beececbe245826",
+    "url": "/movie_app/static/js/2.8d1df91e.chunk.js"
+  },
+  {
+    "revision": "66de514a7cc916716eb3",
+    "url": "/movie_app/static/js/main.a9cb580c.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
